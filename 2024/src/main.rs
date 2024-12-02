bbaec3bf497a1d87a4e249cc970c1f9c858c0121
Wesley Moore <wes@wezm.net>
2024-12-02 10:39:41 +1000

    WIP

---
+++ 2024/src/main.rs
@@ -0,0 +1,24 @@
+use std::env;
+use std::path::{Path, PathBuf};
+
+mod day1;
+
+fn main() {
+    let Some(input) = env::args().skip(1).next().map(PathBuf::from) else {
+        eprintln!("Usage advent-of-code dayN.txt");
+        return;
+    };
+    
+    let day = input.file_stem()
+        .and_then(|stem| stem.to_str())
+        .and_then(|stem| stem.strip_prefix("day").unwrap_or(stem).parse::<u8>().ok());
+    let Some(day) = day else {
+        eprintln!("Unable to determine day from {}", input.display());
+        return;
+    };
+    
+    match day {
+        1 => day1::main(&input),
+        _ => { eprintln!("Unknown day {day}"); return }
+    }
+}
